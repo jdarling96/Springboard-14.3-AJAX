649c1709e93201bc6bf5a3dbff1cbb797ebd36b2
Josh Darling <Joshua.a.darling@gmail.com>
2022-04-03 14:20:00 -0600

    Update XHR/app.js

--- VideoCode/XHR/app.js
+++ VideoCode/XHR/app.js
@@ -1,30 +1,30 @@
-const firstReq = new XMLHttpRequest();
-firstReq.addEventListener('load', function () {
-  const data = JSON.parse(this.responseText);
-  for (let planet of data.results) {
+const firstReq = new XMLHttpRequest();       // Requests take time. Javascript relies on the browser to make the request and tell javascript to run its code
+firstReq.addEventListener('load', function () {  // similiar to setTimeout request utlize callbacks to wait for browsers to run the callback code
+  const data = JSON.parse(this.response)
+  for(let planet of data.results){
     console.log(planet.name)
-  }
-  const nextUrl = data.next;
-  const secondReq = new XMLHttpRequest();
-  secondReq.addEventListener('load', function () {
-    console.log("SUCCESS!");
-    const data = JSON.parse(this.responseText);
-    for (let planet of data.results) {
+  } 
+   const nextUrl = data.next
+   const newUrl = new XMLHttpRequest();
+   newUrl.addEventListener('load', function () {   // in order to get the second page from out starwars api we depend on the firt request
+    const data = JSON.parse(this.response)         // therefor we have to nest our second request and repeat the steps depeding on that first request to pass
+    for(let planet of data.results){
       console.log(planet.name)
-    }
-  });
-  secondReq.addEventListener('error', function () {
-    console.log("ERROR!")
-  });
-  secondReq.open('GET', nextUrl);
-  secondReq.send();
-  console.log("JUST SENT 2nd REQUEST....")
+    } 
 
-
+   })
+   newUrl.addEventListener('error', () => {
+     console.log('error')
+   })
+   newUrl.open('GET', nextUrl)
+   newUrl.send();
 });
+
 firstReq.addEventListener('error', () => {
   console.log('ERROR!!!!!!');
 });
 firstReq.open('GET', 'https://swapi.dev/api/planets/');
 firstReq.send();
-console.log("SENDING REQUEST......")
+console.log("SENDING REQUEST......")           // We will see this first in the log since requests take time
+
+// this is the built in method for browsers we will use AXIOS
