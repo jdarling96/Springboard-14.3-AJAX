19994fbfeda3ac181e31f6e457961ce0624477d0
joshdarling <joshua.a.darling@gmail.com>
2022-04-03 18:39:43 -0600

    axios request pulls space data api and updates DOM

--- VideoCode/axios/app.js
+++ VideoCode/axios/app.js
@@ -1,39 +1,39 @@
-// async function getData() {
-//   const response = await axios.get('https://swapi.dev/api/planets/');
-//   const { next, results } = response.data;
-//   for (let planet of results) {
-//     console.log(planet.name)
-//   }
-//   const response2 = await axios.get(next);
-//   const results2 = response2.data.results;
-//   for (let planet of results2) {
-//     console.log(planet.name)
-//   }
-// }
 
-// getData();
-// console.log("I HAPPEN AFTER getData()")
+/*async function getData () {
+const response = await axios.get('https://swapi.dev/api/planets/')   //cant call await without async. response wont run till our promise is resolved
+const { next, results } = response.data
+console.log('this line is after axios.get'); 
+for(let planet of results) {
+  console.log(planet.name)
+} 
+const response2 = await axios.get(next);
+const results2 = response2.data.results
+for(let planet of results2 ) {
+  console.log(planet.name)
+} 
 
-async function getLaunches() {
+}
+getData();
+*/
+
+ async function getLaunches() {
   const res = await axios.get('https://api.spacexdata.com/v3/launches/upcoming');
-  renderLaunches(res.data);
+  const ul = document.querySelector('#launches')
+ console.log(res)
+ for(let launch of res.data) {
+   const newLI = document.createElement('LI')
+   const mission = document.createElement('B')
+   mission.innerText = launch.mission_name
+   newLI.append(mission)
+   ul.append(newLI)
+   newLI.innerHTML += ` - {launch.rocket.rocket_name}`
+  //newLI.style.fontFamily
+  
+   console.log(launch.mission_name)
+  console.log(launch.rocket.rocket_name)
+}
+  
 }
 
-function renderLaunches(launches) {
-  const ul = document.querySelector("#launches");
-  for (let launch of launches) {
-    ul.append(makeLaunchLI(launch))
-  }
-}
 
-function makeLaunchLI(launch) {
-  const newLI = document.createElement('LI');
-  const mission = document.createElement('B');
-  mission.innerText = launch.mission_name;
-  newLI.append(mission);
-  newLI.innerHTML += ` - ${launch.rocket.rocket_name}`
-  return newLI;
-}
-
-const btn = document.querySelector('#getLaunches');
-btn.addEventListener('click', getLaunches);
+   
